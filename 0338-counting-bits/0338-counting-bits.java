583dbfd60804377818a85951373a5c9c36816bf7
Achuth Bejawada <2300032463@kluniversity.in>
2026-07-13 20:32:21 +0530

    Time: 9 ms (10.83%), Space: 48.4 MB (85.85%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] countBits(int n) {
+        int result[]=new int[n+1];
+        for(int i=0;i<=n;i++){
+            int count = 0;
+            int temp = i;
+            while(temp > 0){
+               if(temp % 2 == 1){
+                count ++;
+               }
+               temp = temp / 2;
+            }
+              result[i] = count;
+              }
+            
+        return result;
+    }
+}
